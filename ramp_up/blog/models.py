ccc58440cf041d8e76426a20542318b832412d95
Hossam ElAnany <HossamShabanAnwar@yahoo.com>
2023-04-07 14:47:50 +0200

    Added 2 new models

---
+++ ramp_up/blog/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length = 100)
+    subtitle = models.CharField(max_length = 100)
+    imgUrl = models.CharField(max_length = 100)
+    body = models.TextField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="author_blogs")
+
+    def __str__(self):
+        return self.title
