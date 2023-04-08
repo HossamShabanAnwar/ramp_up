a4a81560fb4578fe3bdc7cedf863055df524068c
Hossam ElAnany <HossamShabanAnwar@yahoo.com>
2023-04-08 15:37:28 +0200

    Changed the database settings to connect to a postgres database

--- ramp_up/ramp_up/settings.py
+++ ramp_up/ramp_up/settings.py
@@ -75,10 +75,21 @@ WSGI_APPLICATION = 'ramp_up.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ramp_up',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
